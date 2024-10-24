6bb335ede5e154883fc453ff54bc0b46093c2637
Mohamed Moslemani <moslemanomohamed@gmail.com>
2024-10-24 20:25:50 +0300

    recommender function - model use

--- model.py
+++ model.py
@@ -26,3 +26,38 @@ def preprocess_data(self):
         self.user_id_map = dict(zip(self.user_encoder.transform(self.user_encoder.classes_), self.user_encoder.classes_))
         self.anime_id_map = dict(zip(self.anime_encoder.transform(self.anime_encoder.classes_), self.anime_encoder.classes_))
     
+
+def get_confidence(rating):
+
+        if rating == -1:
+            return 1  
+        else:
+            return 1 + rating / 10  
+    
+
+def train_model(self, factors=20, regularization=0.1, iterations=20, use_gpu=False):
+   
+        item_user_data = self.interaction_matrix.T
+        
+        self.model = implicit.als.AlternatingLeastSquares(
+            factors=factors,
+            regularization=regularization,
+            iterations=iterations,
+            use_gpu=use_gpu
+        )
+        
+        self.model.fit(item_user_data)
+        print("Model training completed.")
+
+    
+def recommend_anime(self, user_id, N=10):
+ 
+        if self.model is None:
+            print("Model has not been trained yet.")
+            return None
+        
+        if user_id in self.user_encoder.classes_:
+            user_idx = self.user_encoder.transform([user_id])[0]
+        else:
+            print("User ID not found.")
+            return None
