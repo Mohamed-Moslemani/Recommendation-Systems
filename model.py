6acec2f99ac20a3b8ad71d65b12fa4d803010bdf
Mohamed Moslemani <moslemanomohamed@gmail.com>
2024-10-24 20:09:56 +0300

    changing preprocessing function - encoding and maping data

--- model.py
+++ model.py
@@ -1,34 +1,28 @@
-import pandas as pd 
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from scipy.sparse import csr_matrix
+import implicit
 
 
 class AnimeRecommender:
-    def __init__(self, data_path):
-        self.data = pd.read_csv(data_path)
+    def __init__(self, anime_data_path, ratings_data_path):
+        self.anime_data = pd.read_csv(anime_data_path)
+        self.ratings_data = pd.read_csv(ratings_data_path)
         self.preprocess_data()
-        self.all_genres = self.get_all_genres()
-    
-    def preprocess_data(self):
-        self.data['genre'] = self.data['genre'].fillna('')
-        self.data['genre'] = self.data['genre'].str.split(', ')   
-        self.data['rating'] = pd.to_numeric(self.data['rating'], errors='coerce')
-        self.data['members'] = pd.to_numeric(self.data['members'], errors='coerce')
-        self.data.dropna(subset=['rating', 'members'], inplace=True)
-    
-    def get_all_genres(self):
- 
-        all_genres = set()
-        for genres in self.data['genre']:
-            all_genres.update(genres)
-        return all_genres
-    
-    def get_user_genres(self):
-        print("Available Genres:")
-        print(', '.join(sorted(self.all_genres)))  
-        user_genres = input("\nEnter your preferred genres, separated by commas: ")
-        user_genres = [genre.strip() for genre in user_genres.split(',')]
-        return user_genres
-    
-    def genre_match(self, anime_genres, user_genres):
-        return any(genre in anime_genres for genre in user_genres)
-    
+        self.model = None
+
+
+def preprocess_data(self):
+
+        self.user_encoder = LabelEncoder()
+        self.ratings_data['user_idx'] = self.user_encoder.fit_transform(self.ratings_data['user_id'])
+        self.anime_encoder = LabelEncoder()
+        self.ratings_data['anime_idx'] = self.anime_encoder.fit_transform(self.ratings_data['anime_id'])
+        self.ratings_data['confidence'] = self.ratings_data['rating'].apply(self.get_confidence)
+        self.interaction_matrix = csr_matrix(
+            (self.ratings_data['confidence'], (self.ratings_data['user_idx'], self.ratings_data['anime_idx']))
+        )
+        self.user_id_map = dict(zip(self.user_encoder.transform(self.user_encoder.classes_), self.user_encoder.classes_))
+        self.anime_id_map = dict(zip(self.anime_encoder.transform(self.anime_encoder.classes_), self.anime_encoder.classes_))
     
